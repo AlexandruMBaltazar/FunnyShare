cf33cbf4b4f70217457eab1ac6cd1224c6ddb0f7
AlexandruMBaltazar <alexandru.baltazar16@gmail.com>
2021-08-14 12:45:32 +0100

    File upload controller security

---
+++ funnyshare/src/main/java/com/funnyshare/funnyshare/file/FileUploadController.java
@@ -0,0 +1,16 @@
+package com.funnyshare.funnyshare.file;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/1.0")
+public class FileUploadController {
+
+    @PostMapping("posts/upload")
+    public void uploadForPost() {
+
+    }
+
+}
---
+++ funnyshare/src/test/java/com/funnyshare/funnyshare/FileUploadControllerTest.java
@@ -0,0 +1,81 @@
+package com.funnyshare.funnyshare;
+
+import com.funnyshare.funnyshare.configuration.AppConfiguration;
+import com.funnyshare.funnyshare.user.UserRepository;
+import com.funnyshare.funnyshare.user.UserService;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.*;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import static org.assertj.core.api.Assertions.*;
+import java.io.File;
+import java.io.IOException;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class FileUploadControllerTest {
+    private static final String API_1_0_POSTS_UPLOAD = "/api/1.0/posts/upload";
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    AppConfiguration appConfiguration;
+
+    @BeforeEach
+    public void init() throws IOException {
+        userRepository.deleteAll();
+        testRestTemplate.getRestTemplate().getInterceptors().clear();
+        FileUtils.cleanDirectory(new File(appConfiguration.getFullAttachmentsPath()));
+    }
+
+
+    @Test
+    public void uploadFile_withImageFromAuthorizedUser_receiveOk() {
+        userService.save(TestUtil.createValidUser("user1"));
+        authenticate("user1");
+        ResponseEntity<Object> response = uploadFile(getRequestEntity(), Object.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void uploadFile_withImageFromUnauthorizedUser_receiveUnauthorized() {
+        ResponseEntity<Object> response = uploadFile(getRequestEntity(), Object.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+    }
+
+    public <T> ResponseEntity<T> uploadFile(HttpEntity<?> requestEntity, Class<T> responseType){
+        return testRestTemplate.exchange(API_1_0_POSTS_UPLOAD, HttpMethod.POST, requestEntity, responseType);
+    }
+
+    private HttpEntity<MultiValueMap<String, Object>> getRequestEntity() {
+        ClassPathResource imageResource = new ClassPathResource("profile.png");
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", imageResource);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+        return requestEntity;
+    }
+
+    public void authenticate(String username) {
+        testRestTemplate.getRestTemplate().getInterceptors().add(new BasicAuthenticationInterceptor(username, "P4ssword"));
+    }
+}
