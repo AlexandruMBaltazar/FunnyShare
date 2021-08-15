4383e9c7e42f360724830459e1d3d40eea9f6aa2
AlexandruMBaltazar <alexandru.baltazar16@gmail.com>
2021-08-15 21:45:17 +0100

    File attachment repository

--- funnyshare/src/main/java/com/funnyshare/funnyshare/file/FileAttachmentRepository.java
+++ funnyshare/src/main/java/com/funnyshare/funnyshare/file/FileAttachmentRepository.java
@@ -2,6 +2,9 @@ package com.funnyshare.funnyshare.file;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FileAttachmentRepository extends JpaRepository<FileAttachment, Long> {
+import java.util.Date;
+import java.util.List;
 
+public interface FileAttachmentRepository extends JpaRepository<FileAttachment, Long> {
+    List<FileAttachment> findByDateBeforeAndPostIsNull(Date date);
 }
