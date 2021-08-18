e93a75a4b7c78702733e900c51d66c16b4294ed6
AlexandruMBaltazar <alexandru.baltazar16@gmail.com>
2021-08-18 15:56:17 +0100

    useState in LoginPage

--- funnyshare-react-client/src/pages/LoginPage.js
+++ funnyshare-react-client/src/pages/LoginPage.js
@@ -1,97 +1,93 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Input from "../components/Input";
 import ButtonWithProgress from "../components/ButtonWithProgress";
 import { connect } from "react-redux";
 import * as authActions from "../redux /authActions";
 
-export class LoginPage extends Component {
-  state = {
-    username: "",
-    password: "",
-    apiError: undefined,
-    pendingApiCall: false,
-  };
+export const LoginPage = (props) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [apiError, setApiError] = useState();
+  const [pendingApiCall, setPendingApiCall] = useState(false);
 
-  onChangeUsername = (event) => {
+  const onChangeUsername = (event) => {
     const value = event.target.value;
-    this.setState({ username: value, apiError: undefined });
+    setUsername(value);
+    setApiError();
   };
 
-  onChangePassword = (event) => {
+  const onChangePassword = (event) => {
     const value = event.target.value;
-    this.setState({ password: value, apiError: undefined });
+    setPassword(value);
+    setApiError();
   };
 
-  onClickLogin = () => {
+  const onClickLogin = () => {
     let user = {
-      username: this.state.username,
-      password: this.state.password,
+      username,
+      password,
     };
 
-    this.setState({ pendingApiCall: true });
+    setPendingApiCall(true);
 
-    this.props.actions
+    props.actions
       .postLogin(user)
       .then((response) => {
-        this.setState({ pendingApiCall: false }, () => {
-          this.props.history.push("/");
-        });
+        setPendingApiCall(false);
+        props.history.push("/");
       })
       .catch((error) => {
-        this.setState({ pendingApiCall: false });
         if (error.response) {
-          this.setState({ apiError: error.response.data.message });
+          setPendingApiCall(false);
+          setApiError(error.response.data.message);
         }
       });
   };
 
-  render() {
-    let disableSubmit =
-      this.state.username && this.state.password ? false : true;
+  let disableSubmit = username && password ? false : true;
 
-    return (
-      <div className="container">
-        <h1 className="text-center">Login</h1>
-        <div className="col-12 mb-3">
-          <Input
-            label="Username"
-            type="text"
-            name="username"
-            placeholder="Your username"
-            value={this.state.username}
-            onChange={this.onChangeUsername}
-          />
-        </div>
-
-        <div className="col-12 mb-3">
-          <Input
-            label="Password"
-            type="password"
-            name="password"
-            placeholder="Your password"
-            value={this.state.password}
-            onChange={this.onChangePassword}
-          />
-        </div>
-
-        {this.state.apiError && (
-          <div className="col-12 mb-3">
-            <div className="alert alert-danger">{this.state.apiError}</div>
-          </div>
-        )}
-
-        <div className="text-center">
-          <ButtonWithProgress
-            onClick={this.onClickLogin}
-            disabled={disableSubmit || this.state.pendingApiCall}
-            text="Login"
-            pendingApiCall={this.state.pendingApiCall}
-          ></ButtonWithProgress>
-        </div>
+  return (
+    <div className="container">
+      <h1 className="text-center">Login</h1>
+      <div className="col-12 mb-3">
+        <Input
+          label="Username"
+          type="text"
+          name="username"
+          placeholder="Your username"
+          value={username}
+          onChange={onChangeUsername}
+        />
       </div>
-    );
-  }
-}
+
+      <div className="col-12 mb-3">
+        <Input
+          label="Password"
+          type="password"
+          name="password"
+          placeholder="Your password"
+          value={password}
+          onChange={onChangePassword}
+        />
+      </div>
+
+      {apiError && (
+        <div className="col-12 mb-3">
+          <div className="alert alert-danger">{apiError}</div>
+        </div>
+      )}
+
+      <div className="text-center">
+        <ButtonWithProgress
+          onClick={onClickLogin}
+          disabled={disableSubmit || pendingApiCall}
+          text="Login"
+          pendingApiCall={pendingApiCall}
+        ></ButtonWithProgress>
+      </div>
+    </div>
+  );
+};
 
 LoginPage.defaultProps = {
   actions: {
